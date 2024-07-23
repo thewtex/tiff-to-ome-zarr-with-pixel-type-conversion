23bddbaf78c1e15a9fe79c630dc4c0f8c0c954df
Matt McCormick <matt.mccormick@kitware.com>
2024-07-23 14:07:23 -0400

    Script argument parsing

--- tiff-to-ome-zarr-with-pixel-type-conversion.py
+++ tiff-to-ome-zarr-with-pixel-type-conversion.py
@@ -6,23 +6,20 @@ Convert a TIFF file to an OME-Zarr file with pixel type conversion.
 
 import argparse
 
+from rich import print
+
 import ngff_zarr as nz 
 
-def main(tiff_file, zarr_file, pixel_type):
-    # Input TIFF file
-    tiff_file = "example.tif"
-    # Output OME-Zarr file
-    zarr_file = "example.zarr"
-    # Convert TIFF to OME-Zarr
-    nz.tiff_to_zarr(tiff_file, zarr_file, pixel_type="uint8")
+
+def tiff_to_ome_zarr_with_pixel_type_conversion(tiff_file, zarr_file, pixel_type):
+    ngff_image = nz.cli_input_to_ngff_image(nz.ConversionBackend.TIFF, tiff_file)
+    print(ngff_image)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument("tiff_file", help="Input TIFF file")
     parser.add_argument("zarr_file", help="Output OME-Zarr file")
-    # add choices for the pixel type to be based on the standard numpy dtypes
     parser.add_argument("--pixel_type", help="Pixel type for the output OME-Zarr file", choices=["uint8", "uint16", "uint32", "uint64", "int8", "int16", "int32", "int64", "float16", "float32", "float64"], default="float32")
     args = parser.parse_args()
-    # convert the TIFF file to an OME-Zarr file
 
-    main()
+    tiff_to_ome_zarr_with_pixel_type_conversion(args.tiff_file, args.zarr_file, args.pixel_type)
