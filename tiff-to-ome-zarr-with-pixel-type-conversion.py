6acb385373b9098232334727602b56875597ec9e
Matt McCormick <matt.mccormick@kitware.com>
2024-07-24 11:14:20 -0400

    Add scaling per Tishi's Java example

--- tiff-to-ome-zarr-with-pixel-type-conversion.py
+++ tiff-to-ome-zarr-with-pixel-type-conversion.py
@@ -7,16 +7,24 @@ Convert a TIFF file to an OME-Zarr file with pixel type conversion.
 import argparse
 
 import ngff_zarr as nz
+import numpy as np
 
-
-def tiff_to_ome_zarr_with_pixel_type_conversion(tiff_files, zarr_file, pixel_type):
+def tiff_to_ome_zarr_with_pixel_type_conversion(tiff_files, zarr_file, pixel_type, min_value, max_value, dynamic_range):
     ngff_image = nz.cli_input_to_ngff_image(nz.ConversionBackend.TIFFFILE, tiff_files)
 
-    ngff_image.data = ngff_image.data.astype(pixel_type)
+    pixel_type = np.dtype(pixel_type)
+    if dynamic_range is None:
+        dynamic_range = np.iinfo(pixel_type).max - np.iinfo(pixel_type).min
+    scale = dynamic_range / (max_value - min_value)
+
+    as_float = ngff_image.data.astype(np.float32)
+    scaled = scale * (as_float - min_value)
+    cast = scaled.astype(pixel_type)
+
+    ngff_image.data = cast
 
     multiscales = nz.to_multiscales(ngff_image)
     nz.to_ngff_zarr(zarr_file, multiscales)
-
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description=__doc__)
@@ -39,11 +47,14 @@ if __name__ == "__main__":
         ],
         default="float32",
     )
+    parser.add_argument("--min", help="minimum value to map", default=0.0)
+    parser.add_argument("--max", help="maximum value to map", default=255.0)
+    parser.add_argument("--dynamic-range", help="dynamic range for scaling the min-max, infer from integer pixel types if not specified", default=None)
     parser.add_argument("tiff_files", help="Input TIFF file(s)", nargs="+")
     parser.add_argument("zarr_file", help="Output OME-Zarr file")
 
     args = parser.parse_args()
 
     tiff_to_ome_zarr_with_pixel_type_conversion(
-        args.tiff_files, args.zarr_file, args.pixel_type
-    )
+        args.tiff_files, args.zarr_file, args.pixel_type, args.min, args.max,
+        args.dynamic_range)
